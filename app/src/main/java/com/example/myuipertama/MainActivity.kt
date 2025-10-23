b5aeec5bdb2234d06f29ee8cfd568ada7ed383c4
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-10-23 12:41:14 +0700

    menambahkan MainActivity dengan struktur dasar

--- app/src/main/java/com/example/myuipertama/MainActivity.kt
+++ app/src/main/java/com/example/myuipertama/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyUipertamaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ActivitasPertama(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
