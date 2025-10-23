2d5c0c5b15a61adca3da8c2b7e4116b620b5060b
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-10-24 01:14:07 +0700

    fungsi composable ActivitasPertama

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -19,7 +18,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
@@ -29,64 +27,19 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun ActivitasPertama(modifier: Modifier){
-    Column(modifier = Modifier.padding(top = 100.dp)
+    Column(modifier = Modifier.padding(top=100.dp)
         .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(
-            stringResource(R.string.prodi),
+    ) {
+        Text(stringResource(R.string.prodi),
             fontSize = 35.sp,
-            fontWeight = FontWeight.Bold
-        )
-        Text(
-            stringResource(R.string.univ),
-            fontSize = 22.sp
-        )
-        Spacer(modifier = Modifier.height(22.dp))
-        Card(
-            modifier = Modifier
-                .fillMaxWidth(1f)
-                .padding(12.dp),
+            fontWeight = FontWeight.Bold)
+        Text(stringResource(R.string.univ),
+            fontSize = 20.sp)
+        Spacer(modifier = Modifier.height(25.dp))
+        Card (modifier = Modifier
+            .fillMaxWidth(1f)
+            .padding(12.dp),
             colors = CardDefaults.cardColors(
                 containerColor = Color.DarkGray
-            )
-        ){
-            Row() {
-                val gambar = painterResource(R.drawable.logoUMY)
-                Image(
-                    painter = gambar,
-                    contentDescription = null,
-                    modifier = Modifier.size(100.dp).padding(5.dp)
-                )
-                Spacer(modifier = Modifier.width(30.dp))
-                Column() {
-                    Text(
-                        stringResource(R.string.nama),
-                        fontSize = 30.sp,
-                        fontFamily = FontFamily.Cursive,
-                        color = Color.White,
-                        modifier = Modifier.padding(5.dp)
-                    )
-                    Text(
-                        stringResource(R.string.alamat),
-                        fontSize = 20.sp,
-                        color = Color.Yellow,
-                        modifier = Modifier.padding(top = 10.dp)
-                    )
-                }
-            }
-        }
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-        ){
-            Text(
-                stringResource(R.string.copy),
-                modifier = Modifier
-                    .align ( Alignment.BottomCenter)
-                    .padding(bottom = 50.dp)
-            )
-        }
-    }
-}
-
+            )) {
