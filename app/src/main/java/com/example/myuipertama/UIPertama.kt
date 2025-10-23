1bcdba6bad4b35f42e8c5a77006112ba19d6567d
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-10-24 01:15:12 +0700

    ubah ukuran dan tata letak gambar logo UMY

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -43,3 +43,11 @@ fun ActivitasPertama(modifier: Modifier){
             colors = CardDefaults.cardColors(
                 containerColor = Color.DarkGray
             )) {
+            Row() {
+                val gambar = painterResource(R.drawable.logoumy)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier.size(100.dp).padding(5.dp)
+                )
+
