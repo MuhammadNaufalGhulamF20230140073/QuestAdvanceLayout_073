ad60791c74bf4e6189997636375d705a6b66fcb9
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-10-23 12:45:31 +0700

    menambahkan teks alamat berwarna kuning di bawah nama

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -67,3 +67,12 @@ fun ActivitasPertama(modifier: Modifier){
                         color = Color.White,
                         modifier = Modifier.padding(5.dp)
                     )
+                    Text(
+                        stringResource(R.string.alamat),
+                        fontSize = 20.sp,
+                        color = Color.Yellow,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                }
+            }
+        }
