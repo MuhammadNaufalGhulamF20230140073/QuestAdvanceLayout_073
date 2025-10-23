405fe90af49167a3fefd05d809696f68b8803ca7
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-10-24 01:22:24 +0700

    nambah card hijau untuk kotak ke 3

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -106,5 +106,12 @@ fun ActivitasPertama(modifier: Modifier){
                 }
             }
         }
+        Spacer(modifier = Modifier.height(5.dp))
+        Card (modifier = Modifier
+            .fillMaxWidth(1f)
+            .padding(12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color(color = 0xFF0a5c0a)
+            )) {
 
 
